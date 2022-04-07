5045c3c4bdbdf557ed2717780a377bc85dff89ae
Godwin Iyogun <giyogun@gmail.com>
2022-04-07 19:28:09 +0100

    update listen port

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postsRoute);
 app.use("/api/categories", categoriesRoute);
 
-app.listen("5000", () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend is working");
 });
